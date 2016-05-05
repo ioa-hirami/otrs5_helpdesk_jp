b688b93725e6ca995b180414c030766b9a41ff0b
hirami <hirami@io-architect.com>
2016-05-05 18:21:52 +0900

    メール関連の初期設定の変更
    宛先のMXチェックを無効に
    メール送信機能を無効に

--- ZZZAuto.pm
+++ ZZZAuto.pm
@@ -8,6 +8,8 @@ use utf8;
 sub Load {
     my ($File, $Self) = @_;
 delete $Self->{'PreferencesGroups'}->{'SpellDict'};
+$Self->{'SendmailModule'} =  'Kernel::System::Email::DoNotSendEmail';
+$Self->{'CheckMXRecord'} =  '0';
 delete $Self->{'NodeID'};
 $Self->{'SecureMode'} =  '1';
 }
